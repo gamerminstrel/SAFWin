6e7976c6d7543b8508fd2db5241136b2b38283b4
Gamer Minstrel <gamerminstrel@gmail.com>
2020-12-16 16:42:20 -0500

    Initial Upload of Project.

---
+++ SAFWin/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Management.Automation;
+
+namespace SAFWin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grab_Info_Button_Click(object sender, RoutedEventArgs e)
+        {
+            PowerShell ps = PowerShell.Create();
+            ps.AddCommand("Get-Date");
+            var results = ps.Invoke();
+            Console.WriteLine(results);
+        }
+    }
+}
